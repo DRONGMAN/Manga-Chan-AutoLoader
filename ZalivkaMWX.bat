d64136107ce08337a369c4570212441b520de4e2
DRONGMAN <82755016+DRONGMAN@users.noreply.github.com>
2021-07-26 21:44:19 +0300

    Add files via upload

---
+++ ZalivkaMWX.bat
@@ -0,0 +1,4 @@
+echo off
+title Manga Chan autoLoader
+node L:\DRON_GMAN\Manga\PROGS\Manga-Chan-AutoLoader-master\ZalivkaMWX.js
+pause
---
+++ ZalivkaMangalib.bat
@@ -0,0 +1,4 @@
+echo off
+title Manga Chan autoLoader
+node L:\DRON_GMAN\Manga\PROGS\Manga-Chan-AutoLoader-master\ZalivkaMangalib.js
+pause
